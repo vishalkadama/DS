c343559cc9e77b831078d8e842c5d570af804ab8
VishalKumar Kadma <vishalkumar.kadma@united.com>
2020-07-15 11:52:26 -0500

    1.2

---
+++ 1.2 Anagram.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+/*
+ Check Permutation:
+ 
+ Given two strings,
+ write a method to decide if one is a permutation of the other.
+ */
+
+// Contains Same chars in both the given elements
+
+
+func sameCharInTwoStrings(s1 : String, s2 : String) -> Bool{
+    
+    guard s1.count == s2.count else {
+        return false
+    }
+    let turple = s1.map {($0,1)}
+    var dict = Dictionary (turple, uniquingKeysWith: +)
+    var dictValue = 0
+    for char in s2 {
+        print(char)
+        if  (dict[char] != nil) {
+            print(dictValue)
+            dictValue = dict[char] ?? 0
+            dictValue -= 1
+            dict.updateValue(dictValue, forKey: char)
+        } else {
+            return false
+        }
+    }
+    
+   return dict.filter {$0.value >= 1}.count > 1 ? false : true
+}
+
+func sameChar(s1 : String, s2 : String) -> Bool{
+    
+    guard s1.count == s2.count else {
+        return false
+    }
+    
+    if s1.sorted() == s2.sorted() {
+        return true
+    } else {
+        return false
+    }
+    
+    return false
+}
+
+sameChar(s1: "vishal", s2: "lahisv")
+
+sameCharInTwoStrings(s1: "vishal", s2: "lahisv")
